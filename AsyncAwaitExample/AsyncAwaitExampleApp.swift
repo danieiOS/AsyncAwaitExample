702744e8c3dd43e4e0c6067b288ab1018c67e0b6
songseongwook <softfrog@kakao.com>
2023-10-29 23:01:14 +0900

    Initial Commit

---
+++ AsyncAwaitExample/AsyncAwaitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitExampleApp.swift
+//  AsyncAwaitExample
+//
+//  Created by 송성욱 on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
